228a8b3ef4450a8e8457ff5599fb1f2be8c73779
Atlasdev08 <114253043+Atlasdev08@users.noreply.github.com>
2022-11-27 11:50:34 +0100

    Update test.py

--- test.py
+++ test.py
@@ -3,8 +3,8 @@ import requests
 
 headers = requests.utils.default_headers()
 
-reponse = requests.get(str("https://www.vinted.fr/vetements?catalog%5B%5D=76&order=newest_first&price_to=15&currency=EUR&brand_id%5B%5D=304"))
+reponse = requests.get(str("http://httpbin.org/ip'"))
 
 
-print(reponse.content.decode() )
+print(reponse.texte )
 
