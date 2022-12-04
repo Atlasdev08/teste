35036305d80369cbf546b06f15e7f7bfc4887ff6
Atlasdev08 <114253043+Atlasdev08@users.noreply.github.com>
2022-12-04 20:59:42 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,9 +1,7 @@
-
 import requests
 
-headers = requests.utils.default_headers()
-headers.update({'User-agent': 'Mozilla/5.0'})
-reponse = requests.get(str("https://www.vinted.fr/vetements?catalog%5B%5D=76&order=newest_first&price_to=15&currency=EUR&brand_id%5B%5D=304"), headers=headers)
+
+reponse = requests.get(str("https://www.vinted.fr/vetements?catalog%5B%5D=76&order=newest_first&price_to=15&currency=EUR&brand_id%5B%5D=304"))
 
 
 print(reponse.content.decode() )
