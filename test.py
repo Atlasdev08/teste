a50340e38c788600fd14501174df8369e5e37618
Atlasdev08 <114253043+Atlasdev08@users.noreply.github.com>
2022-12-04 20:56:03 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,9 @@
 
 import requests
-session = requests.session()
-r = session.get("http://httpbin.org/ip")
-print(r.text)
 
+headers = requests.utils.default_headers()
+headers.update({'User-agent': 'Mozilla/5.0'})
+reponse = requests.get(str("https://www.vinted.fr/vetements?catalog%5B%5D=76&order=newest_first&price_to=15&currency=EUR&brand_id%5B%5D=304"), headers=headers)
+
+
+print(reponse.content.decode() )
