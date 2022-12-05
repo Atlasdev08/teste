e2a5fe4334bdfba774c9c9cb8ce4980964e92bb1
Atlasdev08 <114253043+Atlasdev08@users.noreply.github.com>
2022-12-05 22:12:26 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,8 +1,6 @@
-import requests
+import cfscrape
+from bs4 import BeautifulSoup as bs
 
-headers = requests.utils.default_headers()
-headers.update({'User-agent': 'Mozilla/5.0'})
-reponse = requests.get(str("https://www.vinted.fr/vetements?catalog%5B%5D=76&order=newest_first&price_to=15&currency=EUR&brand_id%5B%5D=304"), headers=headers)
-
-
-print(reponse)
+scraper=cfscrape.create_scraper()
+reponse=scraper.get("https://www.vinted.fr/vetements?catalog%5B%5D=76&order=newest_first&price_to=15&currency=EUR&brand_id%5B%5D=304")
+print(reponse.text)
